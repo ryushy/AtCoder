6df473a809ff5b2ae234a1160d2bb51c6500daee
ryushy <ryuyaguchi@gmail.com>
2020-04-13 14:48:51 +0900

    C - Traveling Salesman around Lake

--- a0100/a101.cpp
+++ a0100/a101.cpp
@@ -1,8 +1,32 @@
-# include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
+// C - Traveling Salesman around Lake
 
-int main () {
-    cout << "HelloWorld" <<endl;
-    return 0;
+int main()
+{
+    int K, N;
+    cin >> K >> N;
+    int A[N] ;
+    for (int i = 0; i < N ; i++)
+    {
+        cin >> A[i];
+    }
+    int m = 0;
+
+    for (int i = 0; i < N - 1; i++)
+    {
+        if (i == 0)
+        {
+            int tmp = A[i] + K - A[N - 1];
+            m = max(m, tmp);
+        }
+        else
+        {
+            m = max(m  , A[i + 1] - A[i]);
+        }
+    }
+
+    int mm = m;
+
+    cout << K-mm << endl;
 }
